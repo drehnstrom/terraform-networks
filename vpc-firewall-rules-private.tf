f36bdf90399343facbf2700940869f7a01c5f4e4
drehnstrom <drehnstrom@gmail.com>
2021-04-08 14:29:16 +0000

    Initial commit

---
+++ vpc-firewall-rules-private.tf
@@ -0,0 +1,59 @@
+# allow ssh only from public subnet
+resource "google_compute_firewall" "private-allow-ssh" {
+  name    = "${google_compute_network.private-vpc.name}-allow-ssh"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = [
+    "${var.subnet_cidr_public}"
+  ]
+  target_tags = ["allow-ssh"] 
+}
+
+# allow rdp only from public subnet
+resource "google_compute_firewall" "private-allow-rdp" {
+  name    = "${google_compute_network.private-vpc.name}-allow-rdp"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+  source_ranges = [
+    "${var.subnet_cidr_public}"
+  ]
+  target_tags = ["allow-rdp"] 
+}
+
+# allow ping only from public subnet
+resource "google_compute_firewall" "private-allow-ping" {
+  name    = "${google_compute_network.private-vpc.name}-allow-ping"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = [
+    "${var.subnet_cidr_public}"
+  ]
+}
+
+# Allow internal traffic on all ports
+resource "google_compute_firewall" "private-allow-internal" {
+  name    = "${google_compute_network.private-vpc.name}-allow-internal"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+  source_ranges = [
+    "${var.subnet_cidr_private}"
+  ]
+}
